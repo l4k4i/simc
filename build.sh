65c8c64aad42b1fbc392d2567cdaa0290233a2dd
mherz <maximilian.herz@igd.fraunhofer.de>
2019-03-22 19:50:14 +0100

    removed source call from build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-source ~/Downloads/emsdk/emsdk_env.sh &&
+#source ~/Downloads/emsdk/emsdk_env.sh &&
 emmake cmake . &&
 emmake make &&
 emcc engine/libengine.a libsimc.a -o simc.js -s WASM=1 -s TOTAL_MEMORY=512MB -s ASSERTIONS=1 -s DISABLE_EXCEPTION_CATCHING=0 -s DEMANGLE_SUPPORT=1 -s EXPORTED_FUNCTIONS='["_callSimc","_main"]' -s EXTRA_EXPORTED_RUNTIME_METHODS='["ccall", "cwrap"]' -Oz -v 
